a940d6fccef742eb4e329aad8cd06e5c36b9e052
Nick Lara <52996657+nickalara@users.noreply.github.com>
2020-07-28 14:49:01 -0700

    Add files via upload

---
+++ src/views/Cats.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <PetTable
+     species="cats"
+     :pets="cats"
+    />
+
+    <!-- <h1>Cats for Adoption</h1>
+    <b-table striped hover :items="cats">
+      <template v-slot:cell(name)="data">
+        <router-link :to="`/pets/cats/${data.index}`">
+          {{ data.value }}
+        </router-link>
+      </template>
+    </b-table> -->
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import PetTable from '@/components/PetTable.vue'
+
+export default {
+  components: {
+    PetTable
+  },
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'cats'
+    ])
+  }
+}
+</script>
---
+++ src/views/Dogs.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <PetTable
+     species="dogs"
+     :pets="dogs"
+    />
+
+    <!-- <h1>Dogs for Adoption</h1>
+    <b-table striped hover :items="dogs">
+      <template v-slot:cell(name)="data">
+        <router-link :to="`/pets/dogs/${data.index}`">
+          {{ data.value }}
+        </router-link>
+      </template>
+    </b-table> -->
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import PetTable from '@/components/PetTable.vue'
+
+export default {
+  components: {
+    PetTable
+  },
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'dogs'
+    ])
+  }
+}
+</script>
